1d3ab21176885bbccdae82e3407829f30493ea91
Eli Bosley <ekbosley@gmail.com>
2016-06-19 15:47:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/elibosley/myapplication/DisplayHelpActivity.java
@@ -0,0 +1,34 @@
+package com.example.elibosley.myapplication;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TabHost;
+
+public class DisplayHelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(getString(R.string.help));
+        ab.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_display_help);
+
+        TabHost host = (TabHost) findViewById(R.id.tabHost);
+        host.setup();
+
+        // Help tab
+        TabHost.TabSpec spec = host.newTabSpec("Help");
+        spec.setContent(R.id.help);
+        spec.setIndicator(getString(R.string.help));
+        host.addTab(spec);
+
+        // Author info tab
+        spec = host.newTabSpec("Author Info");
+        spec.setContent(R.id.author_info);
+        spec.setIndicator(getString(R.string.author_tab));
+        host.addTab(spec);
+    }
+}
---
+++ app/src/main/java/com/example/elibosley/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.elibosley.myapplication;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextClock;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public final static String EXTRA_MESSAGE = "com.example.elibosley.myapplication.MESSAGE";
+    public static boolean isTwentyFour = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    public void sendMessage(View view) {
+        // Do something to respond to the button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // creates the menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handles actionbar item clicks here
+        int id = item.getItemId();
+
+        if (id == R.id.help) {
+            System.out.println("DisplayHelpActivity selected");
+            Intent intent = new Intent(this, DisplayHelpActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.textClock) {
+
+            String twelveHour = getString(R.string.time_format_12);
+            String twentyFourHour = getString(R.string.time_format_24);
+            String mode = getString(R.string.time_mode);
+            String current_mode = null;
+            TextClock clock = (TextClock) findViewById(R.id.textClock);
+
+            if (isTwentyFour) {
+                clock.setFormat12Hour(twentyFourHour);
+                clock.setFormat24Hour(twentyFourHour);
+                isTwentyFour = false;
+                current_mode = mode + " " + getString(R.string.time_mode_24);
+            }
+            else {
+                clock.setFormat12Hour(twelveHour);
+                clock.setFormat24Hour(twelveHour);
+                isTwentyFour = true;
+                current_mode = mode + " " + getString(R.string.time_mode_12);
+            }
+
+            Snackbar.make(view, current_mode, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show();
+
+        }
+    }
+}
